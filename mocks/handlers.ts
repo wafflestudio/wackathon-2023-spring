1235d2cf200cc5721d114696b888a1923ab09095
designdefined.by@gmail.com <designdefined.by@gmail.com>
2023-02-17 19:11:39 +0900

    fix: fake db error

--- mocks/handlers.ts
+++ mocks/handlers.ts
@@ -4,8 +4,8 @@ import { UserLoginRequest } from "../entities/user/userLogin";
 
 export const handlers = [
   rest.post<UserLoginRequest>("/login", (req, res, ctx) => {
-    const { id, password } = req.body;
-    if (id === "test" && password === "right") {
+    const { username, password } = req.body;
+    if (username === "test" && password === "right") {
       return res(ctx.status(200), ctx.json(fakeUsers[0]));
     } else {
       return res(ctx.status(403));
